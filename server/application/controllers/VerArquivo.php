188118b9e0080fbf78302b51cba41659a0453862
Kelvin Alves <kelvinpalves@gmail.com>
2017-02-22 23:32:00 -0300

    Ver imagem e pdf da demanda

---
+++ server/application/controllers/VerArquivo.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class VerArquivo extends MY_Controller {
+
+	public function porDemanda() {
+		$idDemanda = $this->uri->segment(3);
+		$idArquivo = $this->uri->segment(5);
+		$retorno = $this->DemandaArquivoModel->buscarArquivosPorIdDemandaEId($idDemanda, $idArquivo);
+		if ($retorno) {
+			$arquivo = file_get_contents($retorno['arquivo']);
+			$fileInfo = new finfo(FILEINFO_MIME_TYPE);
+			$mimeType = $fileInfo->buffer(file_get_contents($retorno['arquivo']));
+			header("content-type: " . $mimeType);
+			print_r($arquivo);
+		} else {
+			print_r("Erro ao carregar o arquivo.");
+		}
+	}
+
+	public function porDemandaFluxo() {
+		$idDemandaFluxo = $this->uri->segment(3);
+		$idArquivo = $this->uri->segment(5);
+		$retorno = $this->DemandaArquivoFluxoModel->buscarArquivosPorIdDemandaEId($idDemanda, $idArquivo);
+		if ($retorno) {
+			$arquivo = file_get_contents($retorno['arquivo']);
+			$fileInfo = new finfo(FILEINFO_MIME_TYPE);
+			$mimeType = $fileInfo->buffer(file_get_contents($retorno['arquivo']));
+			header("content-type: " . $mimeType);
+			print_r($arquivo);
+		} else {
+			print_r("Erro ao carregar o arquivo.");
+		}
+	}
+	
+}
